e4feb01714154e17a2a40cf31bbdffadb5b93e00
Abhinandan Chandra <chandraabhinandan@gmail.com>
2024-07-31 18:27:21 +0530

    About page completed

---
+++ frontend/src/About.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function About(){
+    return (<div>
+        <p className='heading'>WE ARE ROBO ENTHUSIASTS SQUAD, THRILLED BY WHAT WE DO</p>
+        <div className='image'>
+          <div className='gradient-border'>
+              <img src="./src/grpimg.png" alt="" />
+          </div>
+        </div>
+        <p className='sub'>Say Hello To Our Awesome Team</p>
+        <br />
+        <p className='item'>Tech Leads :</p>
+        <div className='members'>
+            <div>
+              <img className='member' src="./src/image.png" alt="" />    
+              <p>Member Name</p>
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>            
+            </div>
+            <div>
+              <img className='member' src="./src/image.png" alt="" />    
+              <p>Member Name</p>   
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>                         
+            </div>   
+            <div>
+              <img className='member' src="./src/image.png" alt="" />   
+              <p>Member Name</p>
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>                             
+            </div>                   
+        </div>
+        <br />
+        <br />
+        <p className='item'>Core Team:</p>
+        <div className='members'>
+            <div>
+              <img className='member' src="./src/image.png" alt="" />    
+              <p>Member Name</p>
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>            
+            </div>
+            <div>
+              <img className='member' src="./src/image.png" alt="" />    
+              <p>Member Name</p>   
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>                         
+            </div>   
+            <div>
+              <img className='member' src="./src/image.png" alt="" />   
+              <p>Member Name</p>
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>                             
+            </div>
+            <div>
+              <img className='member' src="./src/image.png" alt="" />   
+              <p>Member Name</p>
+              <div className='social'>
+              <a href=""><img src="./src/fb.png" alt="" height={"18px"} width={"13px"} /></a>    
+              <a href=""><img src="./src/ig.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/linkedin.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/yt.png" alt="" height={"18px"} width={"13px"} /></a>
+              <a href=""><img src="./src/x.jpg" alt="" height={"18px"} width={"13px"} /></a>
+              </div>                             
+            </div>
+                               
+        </div>
+        <br />
+        <br />
+
+    </div>);
+}
+
+export default About;
+
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.jsx'
 import './index.css'
+import About from './About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <About />
   </React.StrictMode>,
 )
