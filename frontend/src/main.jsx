132c02e86a126f10b855cf5de5eba824ad858e1c
decalcomania22 <abhilashapark22@gmail.com>
2024-08-01 00:44:18 +0530

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import Home from './Home.jsx'
 import './index.css'
 import About from './About.jsx'
+import Blog from './blogs.jsx'
+import BlogPage from './blogspage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <About />
+    <Blog/>
   </React.StrictMode>,
 )
