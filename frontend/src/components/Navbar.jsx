d0498e553cefcac6b81455008071e0ccd37052c1
Shreya-sinha <sinhashreya2679@gmail.com>
2024-08-06 03:05:14 +0530

    feat: Enhance blog layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,31 @@
-import React from 'react'
-import '../pages/blogs.css'
+// import React from 'react'
+import "../pages/blogs.css";
 const Navbar = () => {
   return (
     <>
-     <header className="flex items-center p-4">
-                <a href="#" className="flex-shrink-0">
-                    <img src="https://avatars.githubusercontent.com/u/70689435?s=200&v=4" alt="Logo" className="h-20" />
-                </a>
-                <nav className="flex-grow flex justify-around ml-4">
-                    <a href="/" className="hover:bg-gray-700 p-2">Home</a>
-                    <a href="/home" className="hover:bg-gray-700 p-2">Events</a>
-                    <a href="/achievements" className="hover:bg-gray-700 p-2">Achievements</a>
-                    <a href="/blogs" className="hover:bg-gray-700 p-2">Blogs</a>
-                    <a href="/about" className="hover:bg-gray-700 p-2">About Us</a>
-                    <a href="#" className="gradient-border">
-                    <a href="#">Roboreg</a>
-                    </a>
-                </nav>
-            </header>
+      <header className="flex items-center p-4 justify-center">
+        <a href="#" className="flex-shrink-0">
+          <img
+            src="https://avatars.githubusercontent.com/u/70689435?s=200&v=4"
+            alt="Logo"
+            className="h-20"
+          />
+        </a>
+        <nav className="flex items-center ">
+          <ul className="flex items-center space-x-14 mx-12">
+            <a href="/">Home</a>
+            <a href="/home">Events</a>
+            <a href="/achievements">Achievements</a>
+            <a href="/blogs">Blogs</a>
+            <a href="/about">About Us</a>
+            <a href="#" className="gradient-border">
+            <a href="#">Roboreg</a>
+            </a>
+          </ul>
+        </nav>
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
